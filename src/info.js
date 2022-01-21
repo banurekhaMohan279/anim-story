c9d3416ef7e9c525301244be2bbffbeb92d4706b
banurekhaMohan279 <banurekha279@gmail.com>
2022-01-21 18:52:11 +0530

    Commented out js type toggle and adding simple css method

--- src/info.js
+++ src/info.js
@@ -2,15 +2,16 @@ import {useState, useEffect} from 'react';
 
 function Info(props){
 
-  function showPopup(){
+/* onMouseEnter/Leave
+ function showPopup(){
     let popup = document.querySelector('.popup');
     popup.classList.toggle('hide');
-  }
+  }*/
 
   return (
     <div className = 'info-container'>
+      <img className = 'Info-icon' src = "/info-icon.svg" alt = 'info' />
       <div className = "popup hide"> {props.data} </div>
-      <img className = 'Info-icon' src = "/info-icon.svg" alt = 'info' onMouseEnter = {showPopup} onMouseLeave = {showPopup}/>
     </div>
   )
 
