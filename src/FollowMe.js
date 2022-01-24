0b717c06fca66422340cba6cbea00afc7fe9a27c
banurekhaMohan279 <banurekha279@gmail.com>
2022-01-24 18:43:47 +0530

    Satrter code for follow me js

--- src/FollowMe.js
+++ src/FollowMe.js
@@ -1,7 +1,21 @@
 
 function FollowMe(){
+  /*let el = document.querySelector(".FollowMeTxt");
+  let ms = 1000;
+  let dist = 25;*/
+  function MoveText(e, ms, dist){
+    let el = e.currentTarget;
+    let distance = el.getBoundingClientRect().left + dist;
+    el.style.transform = `translate(${distance}px)`
+    el.style.transitionProperty = 'all';
+    el.style.transitionDuration = ms/1000+'s';
+    el.style.transitionTimingFunction = 'ease';
+    el.style.cursor = 'pointer';
+  }
   return(
-    <p> Follow me page! </p>
+    <div className = 'FollowMeDiv'>
+      <p className = 'FollowMeTxt' onClick = { (e) => MoveText(e, 1000, 50)}> Follow me! </p>
+    </div>
   )
 }
 
